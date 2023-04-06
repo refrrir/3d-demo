51e0f7540991d9acce9445aa245de3b79d4f1a9d
qiang <lqiang@yahoo.com>
2023-04-06 14:42:17 +0800

    Add dev server.
    npm run dev
    http://localhost:8080

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
   resolve: {
     alias: {
       "@constants": path.resolve(__dirname, "src/constants"),
       "@objects": path.resolve(__dirname, "src/objects"),
       "@models": path.resolve(__dirname, "src/models"),
-    }
-  }
+    },
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "static"),
+    },
+  },
 };
